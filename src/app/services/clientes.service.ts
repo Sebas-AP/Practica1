36ae1522b5ef0483a06fd18c2ef040e6d43a2ae8
Sebas-AP <almedasebastian412@hotmail.com>
2025-06-09 19:50:00 -0600

    dist2

--- src/app/services/clientes.service.ts
+++ src/app/services/clientes.service.ts
@@ -8,6 +8,7 @@ export class ClientesService {
 
   constructor(private http: HttpClient) { }
   guardarusuario(usuario: any) {
+    
     return this.http.post(`http://localhost/entretenia/registroUser.php`, JSON.stringify(usuario));
 
   }
